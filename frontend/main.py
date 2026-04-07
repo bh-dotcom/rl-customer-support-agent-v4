e1f85e6e2733c2d8ce04e89cdfa56001bc0b3fdd
Bhoomika M <73037292+bh-dotcom@users.noreply.github.com>
2026-04-07 20:02:27 +0530

    Update main.py

--- frontend/main.py
+++ frontend/main.py
@@ -3,11 +3,11 @@ import requests
 
 st.title("RL Customer Support Agent")
 
-user_query = st.text_input("Enter your query:")
+query = st.text_input("Enter your query:")
 
 if st.button("Send"):
-    resp = requests.post("http://127.0.0.1:8000/step", json={"query": user_query})
-    st.write("Agent Reply:", resp.json()["observation"]["state_text"])
+    response = requests.post("http://127.0.0.1:8000/step", json={"query": query})
+    st.write("Agent Reply:", response.json()["observation"]["state_text"])
 
 def main():
     st.write("Frontend running")
